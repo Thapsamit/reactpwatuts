396f7eae867a907bf07949f0df8510964d98b3c8
Thapsamit <thapliyalamit2001@gmail.com>
2023-03-03 12:12:42 +0530

    third

--- src/components/audio/Audio.js
+++ src/components/audio/Audio.js
@@ -1,20 +1,7 @@
-import AudioPlayer from "react-h5-audio-player";
+//import AudioPlayer from "react-h5-audio-player";
 
 const Audio = () => {
-  return (
-    <AudioPlayer
-      src="https://api.still.ag/media/level1/audios/day-3.mp3"
-      customAdditionalControls={[]}
-      customVolumeControls={[]}
-      showJumpControls={true}
-      layout="stacked-reverse"
-      progressJumpSteps={{
-        forward: 0,
-        backward: 10000,
-      }}
-      hasDefaultKeyBindings={false}
-    />
-  );
+  return <></>;
 };
 
 export default Audio;
