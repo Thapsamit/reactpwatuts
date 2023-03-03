38961ed6948233dd8f6026edc9d5c1a78b35ac6f
Thapsamit <thapliyalamit2001@gmail.com>
2023-03-03 12:10:58 +0530

    second

--- src/components/phone/PhoneInput.js
+++ src/components/phone/PhoneInput.js
@@ -1,14 +1,9 @@
 import React, { useRef, useEffect } from "react";
-import IntlTelInput from "react-intl-tel-input";
-import "react-intl-tel-input/dist/main.css";
+// import IntlTelInput from "react-intl-tel-input";
+// import "react-intl-tel-input/dist/main.css";
 
 const PhoneInput = () => {
-  return (
-    <IntlTelInput
-      containerClassName="intl-tel-input"
-      inputClassName="form-control"
-    />
-  );
+  return <></>;
 };
 
 export default PhoneInput;
